973c7d0e8cc07388e4f715b1e41e195da85f28a4
moavalos <mora_m_avalos@hotmail.com>
2024-04-30 11:14:55 -0300

    formulario con vistas y validacion

--- gs-validating-form-input/src/main/java/com/validating/formvalidation/PersonForm.java
+++ gs-validating-form-input/src/main/java/com/validating/formvalidation/PersonForm.java
@@ -1,4 +1,40 @@
 package com.validating.formvalidation;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class PersonForm {
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String name;
+
+    @NotNull
+    @Min(18)
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonForm{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
